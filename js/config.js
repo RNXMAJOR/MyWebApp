682a7e78b92bf723093675be980622575d593420
RNXMAJOR <oyimaaaron@gmail.com>
2021-04-22 17:03:39 -0500

    first

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_TBpPdhM3D', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6mbp6tjtqk7kh9m40dr75hkp6e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://1ftdyaoqde.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',,
+    }
+};
